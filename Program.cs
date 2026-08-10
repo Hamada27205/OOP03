4d4231b2c13e9edbb46380bf9d97ccc32fd45703
Hamada27205 <ahmedfarag8273@gmail.com>
2026-08-11 00:44:08 +0300

    Question1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Part01
+
+            #region Q1
+
+            //a) Method Overloading means having multiple methods with the same name but different parameters.
+            //Method Overriding means a derived class provides a specific implementation of a method that is already defined in the base class.
+
+            //b) Static Binding (Early Binding) occurs at compile time, while Dynamic Binding (Late Binding) occurs at runtime.
+
+            #endregion
+
+            #endregion
+
         }
     }
 }
